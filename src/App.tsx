d051afa2d66b6eb369e6457b9af133116260a4b3
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-11 02:05:37 -0600

    cssbaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter } from 'react-router-dom'
+import CssBaseline from '@mui/material/CssBaseline'
+
 import NavBar from './components/Nav/NavBar'
 
 export function App() {
   return (
-    <>
+    <CssBaseline>
       <NavBar />
-    </>
+    </CssBaseline>
   )
 }

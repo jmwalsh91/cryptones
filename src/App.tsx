029015c0061c1ba5c2b20f8ce7aae8787c0f4e00
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-11 02:03:07 -0600

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
-
-import { AppRoutes } from '~/routes'
-import { GlobalStyle, theme } from '~/styles'
+import NavBar from './components/Nav/NavBar'
 
 export function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <AppRoutes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <>
+      <NavBar />
+    </>
   )
 }

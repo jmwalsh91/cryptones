fa42f0e3637cacd2f58ea13d6cfa80df93f46f9a
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-14 23:40:27 -0600

    tonecontrols component

---
+++ src/components/toneControls/PlaybackControls.tsx
@@ -0,0 +1,31 @@
+import { Pause, Stop, PlayArrow, MusicNote } from '@mui/icons-material'
+import { IconButton, Paper, ButtonGroup } from '@mui/material'
+import React from 'react'
+
+interface playbackProps {
+  color: 'primary' | 'secondary'
+  iconSize: 'small' | 'medium' | 'large'
+}
+
+function PlaybackControls({ color, iconSize }: playbackProps) {
+  return (
+    <Paper variant="outlined">
+      <ButtonGroup>
+        <IconButton aria-label="MusicNote" color={color} size={iconSize}>
+          <MusicNote />
+        </IconButton>
+        <IconButton aria-label="PlayArrow" color={color} size={iconSize}>
+          <PlayArrow />
+        </IconButton>
+        <IconButton aria-label="Pause" color={color} size={iconSize}>
+          <Pause />
+        </IconButton>
+        <IconButton aria-label="Stop" color={color} size={iconSize}>
+          <Stop />
+        </IconButton>
+      </ButtonGroup>
+    </Paper>
+  )
+}
+
+export default PlaybackControls

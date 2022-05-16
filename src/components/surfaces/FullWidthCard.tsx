a02f3ecff46af0b9ea7dd7191725cef95712eb83
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-15 23:46:49 -0600

    pass data as props to fullwidthcard

--- src/components/surfaces/FullWidthCard.tsx
+++ src/components/surfaces/FullWidthCard.tsx
@@ -1,14 +1,15 @@
 import { Paper, Typography } from '@mui/material'
 
+import { ohlcvData } from '~/types/interfaces'
+
 type Props = {
-  children?: any
+  ohlcvData: ohlcvData
 }
 
-function FullWidthCard({ children }: Props) {
+function FullWidthCard({ ohlcvData }: Props) {
   return (
     <Paper sx={{ width: '100%', height: '100%', backgroundColor: 'black' }}>
-      <Typography variant="h1">cryptones</Typography>
-      {children}
+      <Typography variant="h1">hi</Typography>
     </Paper>
   )
 }

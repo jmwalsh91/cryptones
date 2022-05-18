e0f14f488f038f3937432e39fb0f65e2e9dd715b
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-18 00:04:42 -0600

    SWRconfig @ App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
+import { SWRConfig } from 'swr'
 
 import NavBar from './components/Nav/NavBar'
 import Foundation from './components/layout/Foundation'
 import Display from './components/surfaces/Display'
+import { cryptonesApi } from './services/Axios'
 import { theme } from './styles/Theme'
 
 export function App() {
   return (
     <CssBaseline>
       <ThemeProvider theme={theme}>
-        <NavBar />
-        <Foundation>
-          <Display />
-        </Foundation>
+        <SWRConfig
+          value={{
+            fetcher: (endpoint, config?) => {
+              return cryptonesApi.get(endpoint, config).then((res) => res)
+            },
+          }}
+        >
+          <NavBar />
+          <Foundation>
+            <Display />
+          </Foundation>
+        </SWRConfig>
       </ThemeProvider>
     </CssBaseline>
   )

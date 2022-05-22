5336bc1f42b6cff4970268a504dab99a127412ad
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-21 23:29:17 -0600

    TokenLogo story

---
+++ src/components/stories/TokenLogo.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta } from '@storybook/react'
+
+import SolanaLogo from '../../public/solana-sol-logo.svg'
+import TokenLogo from '../main-view/chart/TokenLogo'
+const meta: Meta = {
+  title: 'Token Logo',
+  component: TokenLogo,
+}
+
+const mockProps = {
+  logo: SolanaLogo,
+}
+
+export default meta
+
+export const Default = () => <TokenLogo logo={SolanaLogo} />

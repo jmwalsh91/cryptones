7d250b4577d5bb1eb334eedd47be9adf4c6fa867
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-22 23:27:00 -0600

    export function to create synth instance w filter

--- src/components/main-view/tone/tone-utils/tone.ts
+++ src/components/main-view/tone/tone-utils/tone.ts
@@ -1,9 +1,13 @@
-/* import * as Tone from 'tone'
+import * as Tone from 'tone'
 
-const pluckedSynth: Tone.Synth<Tone.SynthOptions> =
-  new Tone.Synth().toDestination()
- */
-// default synth for initial config
+export const newSynth = () => {
+  //filter for fun
+  const filter = new Tone.Filter()
+  //default synth
+  const pluckedSynth: Tone.Synth<Tone.SynthOptions> = new Tone.Synth()
+    .connect(filter)
+    .toDestination()
+}
 //apply mappings
 // apply src to init array, get note value
 // put switch on sensitivity

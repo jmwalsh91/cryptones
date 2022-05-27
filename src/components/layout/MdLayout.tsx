b7697c6f430cdd53b05d720e62ed3e6b617a4b88
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-27 01:12:31 -0600

    vite builds successfully

--- src/components/layout/MdLayout.tsx
+++ src/components/layout/MdLayout.tsx
@@ -1,33 +1,26 @@
 import { Container, Grid } from '@mui/material'
-import { SWRConfig } from 'swr'
-import ToneContext from '~/services/ToneContext'
 
 import FullWidthCard from '../main-view/chart/FullWidthCard'
 import MappingsCard from '../main-view/map/MappingsCard'
 import ToneCard from '../main-view/tone/ToneCard'
 
 function MdLayout() {
-  
   return (
     <Container sx={{ my: '1rem' }}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <FullWidthCard />
         </Grid>
-        <ToneContext>
-          <Grid item xs={12} sm={6} md={7}>
-            <MappingsCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={5}>
-            <ToneCard />
-          </Grid>
-        </ToneContext>
+
+        <Grid item xs={12} sm={6} md={7}>
+          <MappingsCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={5}>
+          <ToneCard />
+        </Grid>
       </Grid>
     </Container>
   )
 }
 
 export default MdLayout
-
-userIsAuthorized 
-if (userIsAuthorized) { }

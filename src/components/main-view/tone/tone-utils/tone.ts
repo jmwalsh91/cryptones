d31ead2d5c7961c1872d2e7570e538fa1b153b77
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-28 23:34:36 -0600

    stronger typing on differenceArray tone util

--- src/components/main-view/tone/tone-utils/tone.ts
+++ src/components/main-view/tone/tone-utils/tone.ts
@@ -33,15 +33,17 @@ export const mapDataToSequence = (synth: Tone.FMSynth, notes: number[]) => {
 //TODO: add typings, Partial<something<something,something>>
 export function differenceArray(data: Array<any>) {
   console.log('props data is ' + data)
-  const difArray: any = []
-  const arr = data.reduce((_previousValue, _currentValue, _ind) => {
-    if (data[_ind][1][3] === data[0][1][3]) {
-      return console.log('first pos')
-    } else {
-      const dif = data[_ind][1][3] - data[_ind - 1][1][3]
-      return difArray.push(Math.abs(Math.trunc(dif)))
-    }
-  }, 0)
-  console.log(arr)
+  const difArray: number[] = []
+  const arr: number[] = data.reduce(
+    (_previousValue: number, _currentValue: number, _ind: number) => {
+      if (data[_ind][1][3] === data[0][1][3]) {
+        return
+      } else {
+        const dif = data[_ind][1][3] - data[_ind - 1][1][3]
+        return difArray.push(Math.abs(Math.trunc(dif)))
+      }
+    },
+    0
+  )
   return difArray
 }

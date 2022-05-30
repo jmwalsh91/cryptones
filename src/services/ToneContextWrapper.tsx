0046ca45d37cbe6f8dad37485b0f979212057f00
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-30 02:17:23 -0600

    added dispatcher for setEndpoint, added endpoint to toneDataContext

--- src/services/ToneContextWrapper.tsx
+++ src/services/ToneContextWrapper.tsx
@@ -19,25 +19,33 @@ export interface toneDataContext {
   source: string
   sensitivity: number
   target: string
+  endpoint: string
 }
 export interface toneDataDispatcher {
   setSource: React.Dispatch<React.SetStateAction<string>>
   setSensitivity: React.Dispatch<React.SetStateAction<number>>
   setTarget: React.Dispatch<React.SetStateAction<string>>
 }
+export interface chartDataDispatcher {
+  setEndpoint: React.Dispatch<React.SetStateAction<string>>
+}
+
 const ToneDataContext = createContext<toneDataContext | null>(null)
 const ToneDataDispatch = createContext<toneDataDispatcher | null>(null)
+const ChartDataDispatch = createContext<chartDataDispatcher | null>(null)
 
 function ToneDataProvider({ children }: Props) {
   const [source, setSource] = useState<string>('default')
   const [sensitivity, setSensitivity] = useState<number>(100)
   const [target, setTarget] = useState<string>('default')
+  const [endpoint, setEndpoint] = useState<string>('/api/ohlcv/')
 
-  const toneDataContext = useMemo(
+  const toneDataContext: toneDataContext = useMemo(
     () => ({
       source: source,
       sensitivity: sensitivity,
       target: target,
+      endpoint: endpoint,
     }),
     [source, sensitivity, target]
   )
@@ -51,11 +59,20 @@ function ToneDataProvider({ children }: Props) {
     [setSource, setSensitivity, setTarget]
   )
 
+  const dispatchChartData = useMemo(
+    () => ({
+      setEndpoint: setEndpoint,
+    }),
+    [setEndpoint]
+  )
+
   return (
     <ToneDataDispatch.Provider value={dispatchToneData}>
-      <ToneDataContext.Provider value={toneDataContext}>
-        {children}
-      </ToneDataContext.Provider>
+      <ChartDataDispatch.Provider value={dispatchChartData}>
+        <ToneDataContext.Provider value={toneDataContext}>
+          {children}
+        </ToneDataContext.Provider>
+      </ChartDataDispatch.Provider>
     </ToneDataDispatch.Provider>
   )
 }
@@ -64,3 +81,4 @@ export default ToneDataProvider
 
 export const useDispatch = () => useContext(ToneDataDispatch)
 export const useToneContext = () => useContext(ToneDataContext)
+export const useChartDataDispatch = () => useContext(ChartDataDispatch)

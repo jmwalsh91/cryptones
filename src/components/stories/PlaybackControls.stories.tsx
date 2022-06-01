07d026df2bb830a31656732041a10b4032d3e577
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-31 23:42:12 -0600

    updated playbackcontrols story

--- src/components/stories/PlaybackControls.stories.tsx
+++ src/components/stories/PlaybackControls.stories.tsx
@@ -1,6 +1,10 @@
 import { Meta } from '@storybook/react'
+import * as Tone from 'tone'
 
 import PlaybackControls from '../main-view/tone/tone-controls/PlaybackControls'
+import { stopPlayback } from '../main-view/tone/tone-utils/tone'
+
+import type * as interfaces from '../../types/interfaces'
 
 const meta: Meta = {
   title: 'Playback Controls',
@@ -9,6 +13,16 @@ const meta: Meta = {
 
 export default meta
 
+const playSynth = () => {
+  console.log('play synth')
+  if (Tone.context.state) {
+    return null
+  } else console.error('error')
+}
+const controls: interfaces.audioControls = {
+  startPlayback: playSynth,
+  stopPlayback: stopPlayback,
+}
 export const Default = () => (
-  <PlaybackControls iconSize="large" color="secondary" />
+  <PlaybackControls iconSize="large" color="secondary" controls={controls} />
 )

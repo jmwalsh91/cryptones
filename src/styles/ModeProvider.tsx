11d899d74559e09778a8570b8d05f2b1f093ecae
jmwalsh91 <jordanmwalsh@gmail.com>
2022-06-03 02:08:01 -0600

    ModeProvider

---
+++ src/styles/ModeProvider.tsx
@@ -0,0 +1,37 @@
+import { PaletteMode, ThemeProvider } from '@mui/material'
+import React, { createContext, ReactNode, useMemo, useState } from 'react'
+
+import * as themeObject from '../styles/Theme'
+
+type Props = {
+  children: ReactNode
+}
+
+const ModeContext = createContext<{ toggleColorMode: () => void }>({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  toggleColorMode: () => {},
+})
+
+export function ModeProvider({ children }: Props) {
+  const [mode, setMode] = useState<PaletteMode>('light')
+
+  const colorMode = useMemo(
+    () => ({
+      // The dark mode switch would invoke this method
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === 'light' ? 'dark' : 'light'
+        )
+      },
+    }),
+    []
+  )
+
+  const theme = mode === 'light' ? themeObject.theme : themeObject.darkTheme
+
+  return (
+    <ModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ModeContext.Provider>
+  )
+}

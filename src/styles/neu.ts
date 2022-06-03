51a1d8c3412ad8b6d209d81d42f1c8859cdcd595
jmwalsh91 <jordanmwalsh@gmail.com>
2022-06-03 03:18:44 -0600

    light and dark style objects with css literals

--- src/styles/neu.ts
+++ src/styles/neu.ts
@@ -1,73 +1,18 @@
 /** @jsx jsx */
 import { css } from '@emotion/react'
 
-export const raisedlight = css`
-  border-radius: 20px;
-  background: #d9d9d9;
-  box-shadow: 6px 6px 15px #cacaca, -6px -6px 15px #e8e8e8;
-`
-export const depressedlight = css`
-  border-radius: 20px;
-  background: #d9d9d9;
-  box-shadow: inset 6px 6px 15px #cacaca, inset -6px -6px 15px #e8e8e8;
-`
-/* export const token = css`
-  display: block;
-  padding: 1rem;
-  margin: 0 auto;
-  width: 6rem;
-  height: 6rem;
-  background-position: 50% 50%;
-  border-radius: 100px;
-  background: #d9d9d9;
-  box-shadow: 6px 6px 15px #cacaca, -6px -6px 15px #e8e8e8;
-` */
-
-export const circleActionlight = css`
-  display: block;
-  padding: 1rem;
-  margin: 0 auto;
-  width: 6rem;
-  height: 6rem;
-  background-position: 50% 50%;
-  border-radius: 100px;
-  background: #d9d9d9;
-  box-shadow: 6px 6px 15px #cacaca, -6px -6px 15px #e8e8e8;
-`
-
-export const actionHoverlight = css`
-  &:hover {
-    border-radius: 100px;
+export const light = {
+  raisedl: css`
+    border-radius: 20px;
+    background: #d9d9d9;
+    box-shadow: 6px 6px 15px #cacaca, -6px -6px 15px #e8e8e8;
+  `,
+  depressedl: css`
+    border-radius: 20px;
     background: #d9d9d9;
     box-shadow: inset 6px 6px 15px #cacaca, inset -6px -6px 15px #e8e8e8;
-  }
-`
-export const actionHoverPrimarylight = css`
-  &:hover {
-    border-radius: 100px;
-    background: #d9d9d9;
-    box-shadow: inset 6px 6px 15px #cacaca, inset -6px -6px 15px #e8e8e8,
-      inset -3px 3px 15px 3px #65abba;
-  }
-`
-
-export const pendingSection = css`
-  opacity: 0.6;
-`
-
-//dark mode literals
-
-export const raiseddark = css`
-  border-radius: 20px;
-  background: #2f2c2c;
-  box-shadow: 17px 17px 34px #1b1919, -17px -17px 34px #433f3f;
-`
-export const depresseddark = css`
-  border-radius: 20px;
-  background: #2f2c2c;
-  box-shadow: inset 6px 6px 12px #181717, inset -6px -6px 12px #464141;
-`
-/* export const token = css`
+  `,
+  /*  token: css`
   display: block;
   padding: 1rem;
   margin: 0 auto;
@@ -79,7 +24,51 @@ export const depresseddark = css`
   box-shadow: 6px 6px 15px #cacaca, -6px -6px 15px #e8e8e8;
 ` */
 
-export const circleActiondark = css`
+  circleActionl: css`
+    display: block;
+    padding: 1rem;
+    margin: 0 auto;
+    width: 6rem;
+    height: 6rem;
+    background-position: 50% 50%;
+    border-radius: 100px;
+    background: #d9d9d9;
+    box-shadow: 6px 6px 15px #cacaca, -6px -6px 15px #e8e8e8;
+  `,
+
+  actionHoverl: css`
+    &:hover {
+      border-radius: 100px;
+      background: #d9d9d9;
+      box-shadow: inset 6px 6px 15px #cacaca, inset -6px -6px 15px #e8e8e8;
+    }
+  `,
+  actionHoverPrimaryl: css`
+    &:hover {
+      border-radius: 100px;
+      background: #d9d9d9;
+      box-shadow: inset 6px 6px 15px #cacaca, inset -6px -6px 15px #e8e8e8,
+        inset -3px 3px 15px 3px #65abba;
+    }
+  `,
+
+  pendingSection: css`
+    opacity: 0.6;
+  `,
+}
+//dark mode literals
+export const dark = {
+  raised: css`
+    border-radius: 20px;
+    background: #2f2c2c;
+    box-shadow: 17px 17px 34px #1b1919, -17px -17px 34px #433f3f;
+  `,
+  depressed: css`
+    border-radius: 20px;
+    background: #2f2c2c;
+    box-shadow: inset 6px 6px 12px #181717, inset -6px -6px 12px #464141;
+  `,
+  /*  token: css`
   display: block;
   padding: 1rem;
   margin: 0 auto;
@@ -87,18 +76,30 @@ export const circleActiondark = css`
   height: 6rem;
   background-position: 50% 50%;
   border-radius: 100px;
-  background: #2f2c2c;
-  box-shadow: 8px 8px 15px #201e1e, -8px -8px 15px #3e3a3a;
-`
+  background: #d9d9d9;
+  box-shadow: 6px 6px 15px #cacaca, -6px -6px 15px #e8e8e8;
+` */
 
-export const actionHoverdark = css`
-  &:hover {
+  circleAction: css`
+    display: block;
+    padding: 1rem;
+    margin: 0 auto;
+    width: 6rem;
+    height: 6rem;
+    background-position: 50% 50%;
     border-radius: 100px;
     background: #2f2c2c;
     box-shadow: 8px 8px 15px #201e1e, -8px -8px 15px #3e3a3a;
-  }
-`
-export const actionHoverPrimarydark = css`
+  `,
+
+  actionHover: css`
+    &:hover {
+      border-radius: 100px;
+      background: #2f2c2c;
+      box-shadow: 8px 8px 15px #201e1e, -8px -8px 15px #3e3a3a;
+    }
+  `,
+  actionHoverPrimary: css`
   &:hover {
     border-radius: 50%;
     background: #2f2c2c;
@@ -106,4 +107,5 @@ export const actionHoverPrimarydark = css`
                 inset -8px -8px 15px #433e3e;
                 inset -3px 3px 15px 3px #65abba;
   }
-`
+`,
+}
